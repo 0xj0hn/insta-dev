8bece09d3242236ac37da7fc9a5221b75b9932de
j0hnr3d <John Red>
2021-08-16 23:34:03 +0430

    add a page for account and add getx to my project

--- App/insta_dev/lib/main.dart
+++ App/insta_dev/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
   runApp(MyApp());
 }
 
@@ -28,6 +30,11 @@ class MainPage extends StatelessWidget {
         child: Column(
           children: [Container(), Container()],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+        tooltip: "اضافه  کردن اکانت...",
       ),
     );
   }
